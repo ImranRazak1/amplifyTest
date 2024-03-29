1cdb76e7606ec528223bdd0a78727d887dde1686
Imran Razak <77019152+ImranRazak1@users.noreply.github.com>
2024-03-29 21:46:00 +0000

    Initial Commit

---
+++ amplifyTest/amplifyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  amplifyTestApp.swift
+//  amplifyTest
+//
+//  Created by Imran razak on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct amplifyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
